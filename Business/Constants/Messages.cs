fd1c601cce4155ac824cacea5cf3f372d8177054
İskender Boynuz <iskenderboynuz90@gmail.com>
2021-02-18 19:41:53 +0300

    18.02.2021 Commit

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -7,9 +7,9 @@ namespace Business.Constants
 {
     public static class Messages
     {
-        public static string CarAdded = "Ürün eklendi";
-        public static string CarNameInvalid = "Ürün ismi geçersiz";
+        public static string CarAdded = "Araç Eklendi";
+        public static string CarNameInvalid = "Araç ismi geçersiz";
         public static string MaintenanceTime ="Sistem bakımda";
-        public static string CarsListed ="Ürünler listelendi";
+        public static string CarsListed ="Araçlar listelendi";
     }
 }
